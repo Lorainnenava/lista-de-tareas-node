05f5aff025cfc562d4c3d886fd3566b977fe9884
Lorainnenava <lorainnenavarro0723@gmail.com>
2023-02-07 16:33:45 -0500

    token agregado

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const app = express();
-const port = 8090;
+const port = 8080;
 const json = require("./data.json");
 const editarTarea = require("./models/list-edit-router");
 const agregarTarea = require("./models/list-agregar-router");
 const eliminarTarea = require("./models/list-eliminar-router");
 const estado = require("./models/list-view-router");
-const {verificacion, validarUrl}= require('./middleware/middleware')
+const { verificacion  , validarUrl } = require("./middleware/middleware");
 
 
-app.use(verificacion)
+
+app.use(verificacion);
 app.use(validarUrl);
 app.use("/editar", editarTarea);
 app.use("/agregar", agregarTarea);
 app.use("/eliminar", eliminarTarea);
 app.use("/estado", estado);
-
 
 app.get("/", function (req, res) {
   res.json(json);
--- models/list-agregar-router.js
+++ models/list-agregar-router.js
@@ -3,9 +3,13 @@ const agregar = express.Router();
 const fs = require("fs");
 const json = require("../data.json");
 const {errores, validacion} = require("../middleware/middleware");
-
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 agregar.use(express.json());
+
+const users = [{ name: "lorainne" }, { name: "Angel" }];
+
 //FUNCION DE AGREGAR
 agregar.post("/",errores, validacion,function (req, res) {
   const datos = req.body;
@@ -16,6 +20,35 @@ agregar.post("/",errores, validacion,function (req, res) {
   });
   res.send("recibido");
   res.end();
+});
+
+//LOGIN
+agregar.post("/login", errores, function (req, res) {
+  const userInfo = users.map((user) => {
+    if (user.name == req.body.name) {
+      return user;
+    }
+  });
+  if (userInfo.length === 0) {
+    res.status(401).send({ error: "Invalid user name or password" });
+  } else {
+    const token = jwt.sign(userInfo[0], process.env.SECRET_KEY, {
+      algorithm: "HS256",
+    });
+
+    res.json({ token });
+  }
+});
+
+agregar.get("/proteger",function (req, res) {
+  const token = req.header("Authorization");
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    console.log(decoded);
+    res.send("autenticado");
+  } catch (error) {
+    res.json({ error });
+  }
 });
 
 module.exports = agregar;
